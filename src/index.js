15fccd2f6550ef0b92e3ae6eaa39e870e01e1df0
qkwn44 <qkwn44@gmail.com>
2022-02-16 19:49:32 +0900

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,60 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { StrictMode, useState } from "react";
+import ReactDOM from "react-dom";
 
+//hooks
+function App() {
+  //useState : 초기 state는 InitialState를 세팅할 수 있ㄴㄴ 옵션 제공
+  //useState는 Array를 return , 첫번재 요소 item 두번째 요소 setItem
+  //setItem : item의 값 변경해주는 기능
+  const [item, setItem] = useState(1);
+  const incrementItem = () => setItem(item + 1);
+  const decrementItem = () => setItem(item - 1);
+  return (
+    <div className="App">
+      <h1>Hello {item}</h1>
+      <h2>Start editing to dee some magic happen! </h2>
+      <button onClick={incrementItem}>Increment</button>
+      <button onClick={decrementItem}>decrementItem</button>
+    </div>
+  );
+}
+
+//class형 컴포넌트 : this, setState 사용
+class AppUgly extends React.Component {
+  state = {
+    item: 1,
+  };
+  render() {
+    const { item } = this.state;
+    return (
+      <div className="App">
+        <h1>Hello {item}</h1>
+        <h2>Start editing to dee some magic happen! </h2>
+        <button onClick={this.incrementItem}>Increment</button>
+        <button onClick={this.decrementItem}>decrementItem</button>
+      </div>
+    );
+  }
+  incrementItem = () => {
+    this.setState((state) => {
+      return {
+        item: state.item + 1,
+      };
+    });
+  };
+  decrementItem = () => {
+    this.setState((state) => {
+      return {
+        item: state.item - 1,
+      };
+    });
+  };
+}
+
+const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <StrictMode>
+    <AppUgly />
+  </StrictMode>,
+  rootElement
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
